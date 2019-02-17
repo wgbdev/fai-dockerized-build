93740a83b0b3db3195a338aca12bba08d6abe816
W. Glen Boyd <glen@wgbdev.com>
2019-02-17 14:42:51 -0800

    remove duplicate script

--- run-it.sh
+++
@@ -1,5 +0,0 @@
-echo
-echo "Starting the setup..."
-echo "--------------------------------"
-docker run --name fai-container --rm -e REPO=pa.archive.ubuntu.com -v ~/fai:/srv/fai/config --privileged -it wgbdev/fai-image
-echo
